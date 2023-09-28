e13ab22e835e18fee7d365ad73da30aec245bf24
demerara151 <demeraraonfire@gmail.com>
2023-09-28 15:43:58 +0900

    introduce httpx and selectolax

--- src/context_manager.py
+++ src/context_manager.py
@@ -1,9 +1,12 @@
 # type: ignore
 """Example of scraping using context manager with selenium manager"""
 from selenium import webdriver
+import asyncio
 from selenium.webdriver.common.by import By
 from dataclasses import dataclass
 from rich import print
+from selectolax import parser
+import httpx
 
 
 @dataclass()
@@ -11,32 +14,39 @@ class ChromeBot:
     url: str
     timeout: float
 
-    def extract_account_names(self, driver: webdriver.Chrome) -> list[str]:
+    def extract_account_info(
+        self, driver: webdriver.Chrome
+    ) -> list[dict[str, str]]:
         "Extract account information from top page banner."
         driver.get(self.url)
-        accounts = driver.find_elements(
-            By.CSS_SELECTOR, ".postListBig .accountName"
-        )
-        return [account.text for account in accounts]
+        accounts = driver.find_elements(By.CSS_SELECTOR, ".postListBig li")
+        return [
+            {
+                "name": account.find_element(
+                    By.CSS_SELECTOR, ".accountName"
+                ).text,
+                "URL": account.find_element(
+                    By.CSS_SELECTOR, "a"
+                ).get_attribute("href"),
+            }
+            for account in accounts
+        ]
 
-    def extract_account_page_urls(self, driver: webdriver.Chrome) -> list[str]:
-        "Extract account information from top page banner."
-        driver.get(self.url)
-        accounts = driver.find_elements(By.CSS_SELECTOR, ".postListBig a")
-        return [account.get_attribute("href") for account in accounts]
 
-    def extract_photo_url(
-        self, driver: webdriver.Chrome, page_url: str
-    ) -> list[str]:
-        "Return URLs of the photo."
-        driver.get(page_url)
-        photos = driver.find_elements(By.CSS_SELECTOR, "figure img")
-        return [photo.get_attribute("src") for photo in photos]
-
-    def save_photo(self, photos: list[bytes]) -> None:
-        for i, photo in enumerate(photos):
-            with open(f"{i:02}_photo.jpg", "wb") as f:
-                f.write(photo)
+@dataclass()
+class Extractor:
+    async def save_photo(self, database: list[dict[str, str]]) -> None:
+        for i, account in enumerate(database):
+            async with httpx.AsyncClient() as client:
+                response = await client.get(account["URL"])
+                tree = parser.HTMLParser(response.text)
+                nodes = tree.css("figure img")
+                for node in nodes:
+                    img_url = node.attributes["src"]
+                    response = await client.get(img_url)
+                    photo = response.content
+                    with open(f"{i:02}_{account['name']}.jpg", "wb") as f:
+                        f.write(photo)
 
 
 if __name__ == "__main__":
@@ -47,7 +57,8 @@ if __name__ == "__main__":
 
     with webdriver.Chrome(options) as driver:
         driver.implicitly_wait(instagram.timeout)
-        names = instagram.extract_account_names(driver)
-        urls = instagram.extract_account_page_urls(driver)
-        account_data = dict(zip(names, urls))
-        print(account_data)
+        accounts = instagram.extract_account_info(driver)
+        print(accounts)
+
+    extractor = Extractor()
+    asyncio.run(extractor.save_photo(accounts))
