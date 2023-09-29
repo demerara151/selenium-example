7ac044cc9367ad782a152255dab6eeeac5b97391
demerara151 <demeraraonfire@gmail.com>
2023-09-29 13:30:06 +0900

    fix: img_url never gonna be a None

--- src/insta_bot.py
+++ src/insta_bot.py
@@ -8,13 +8,17 @@ from rich import print
 import httpx
 from selectolax.parser import HTMLParser
 
+URL: str = "https://instagrammernews.com/"
+options = webdriver.ChromeOptions()
+options.add_argument("headless=new")  # type:ignore
+
 
 @dataclass()
 class ChromeBot:
     url: str
 
-    def extract_account_info(self, driver: webdriver.Chrome) -> list[str]:
-        "Extract account page URL from top page banner."
+    def extract_post_urls(self, driver: webdriver.Chrome) -> list[str]:
+        "Extract all post page URLs from top page banner."
         driver.get(self.url)
         links = driver.find_elements(By.CSS_SELECTOR, ".postListBig li a")
         return [link.get_attribute("href") for link in links]  # type: ignore
@@ -22,7 +26,7 @@ class ChromeBot:
 
 @dataclass()
 class Extractor:
-    async def get_html(self, page_url: str) -> str:
+    async def fetch_html(self, page_url: str) -> str:
         """
         Retrieve HTML from their profile page.
 
@@ -34,35 +38,30 @@ class Extractor:
             html = response.text
             return html
 
-    async def extract_img_url(
-        self, html: str
-    ) -> dict[str, str | list[str | None]]:
+    async def extract_img_url(self, html: str) -> dict[str, str | list[str]]:
         "Extract user name and list of image URLs from HTML"
         tree = HTMLParser(html)
         images = tree.css("figure img")
         username = tree.css_first(".accountName > strong > a")
         return {
             "name": username.text(),
-            "links": [image.attributes["src"] for image in images],
+            "links": [image.attributes["src"] or "" for image in images],
         }
 
-    async def fetch_img(self, img_url: str | None) -> bytes | None:
+    async def fetch_img(self, img_url: str) -> bytes:
         """
         Fetch image data from extracted image URL.
 
         ### Parameter:
         img_url: URL of the image.
         """
-        if img_url:
-            async with httpx.AsyncClient(timeout=5.0) as client:
-                response = await client.get(img_url)
-                img = response.content
-                await asyncio.sleep(0.5)
-                return img
-        else:
-            return None
+        async with httpx.AsyncClient(timeout=5.0) as client:
+            response = await client.get(img_url)
+            img = response.content
+            await asyncio.sleep(0.1)
+            return img
 
-    async def save_img(self, name: str, img: bytes | None) -> None:
+    async def save_img(self, name: str, img: bytes) -> None:
         """
         Save image as account name with serial number.
 
@@ -70,39 +69,20 @@ class Extractor:
         name: User name.
         img: Image data must be byte object.
         """
-        if img:
-            print(f"Write image to {name}")
-            with open(name, "wb") as f:
-                f.write(img)
-        else:
-            print(
-                """This user doesn't have any images. This gonna never happen.
-                Please check your code."""
-            )
+        print(f"Write image to {name}")
+        with open(name, "wb") as f:
+            f.write(img)
 
-    async def main(self, profile_pages: list[str]):
+    async def main(self, profile_pages: list[str]) -> None:
         """
-        1. Access to each profile pages asynchronously.
-        2. Create the database that contains
-            user name and URL list of the images.
-        3. Download those images and save it
-            with `{username}_{uuid}.jpg` format.
-
-        Gather these tasks and run asynchronously.
+        Download all images and save it
+        with `{username}_{uuid}.jpg` format.
         """
         # Asynchronously fetch the HTML for each profile page
-        async with asyncio.TaskGroup() as tg:
-            html_documents = [
-                await tg.create_task(self.get_html(page_url))
-                for page_url in profile_pages
-            ]
+        html_documents = await self.fetch_all_html(profile_pages)
 
         # Extract the user name and image URLs from the HTML
-        async with asyncio.TaskGroup() as tg:
-            database = [
-                await tg.create_task(self.extract_img_url(html))
-                for html in html_documents
-            ]
+        database = await self.create_database(html_documents)
 
         # Asynchronously download and save the images
         async with asyncio.TaskGroup() as tg:
@@ -117,18 +97,50 @@ class Extractor:
                 for img_url in record["links"]
             ]
 
+    async def create_database(
+        self, html_documents: list[str]
+    ) -> list[dict[str, str | list[str]]]:
+        """
+        Create the database contains the user name and image URLs
+        by extracting them from the each HTML documents.
+        """
+        try:
+            async with asyncio.TaskGroup() as tg:
+                database = [
+                    await tg.create_task(self.extract_img_url(html))
+                    for html in html_documents
+                ]
+        except* Exception as eg:
+            for error in eg.exceptions:
+                print(error)
+            raise
+        else:
+            return database
+
+    async def fetch_all_html(self, profile_pages: list[str]) -> list[str]:
+        # Asynchronously fetch the HTML for each profile page
+        try:
+            async with asyncio.TaskGroup() as tg:
+                html_documents = [
+                    await tg.create_task(self.fetch_html(page_url))
+                    for page_url in profile_pages
+                ]
+        except* Exception as eg:
+            for error in eg.exceptions:
+                print(error)
+            raise
+        else:
+            return html_documents
+
 
 if __name__ == "__main__":
-    URL: str = "https://instagrammernews.com/"
     instagram = ChromeBot(URL)
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless=new")  # type:ignore
 
     with webdriver.Chrome(options) as driver:
         driver.implicitly_wait(5.0)
-        pages = instagram.extract_account_info(driver)
-        print(pages)
+        urls = instagram.extract_post_urls(driver)
+        print(urls)
 
     print("Start to extract images.")
     extractor = Extractor()
-    asyncio.run(extractor.main(pages))
+    asyncio.run(extractor.main(urls))
